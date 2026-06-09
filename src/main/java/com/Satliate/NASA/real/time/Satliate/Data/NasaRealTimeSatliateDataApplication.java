f365cda9a442ca1a5c7069298ba0450e5f5db11b
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2026-06-09 11:26:45 +0400

    The NASA Sataliate project has been created it successfully

---
+++ src/main/java/com/Satliate/NASA/real/time/Satliate/Data/NasaRealTimeSatliateDataApplication.java
@@ -0,0 +1,13 @@
+package com.Satliate.NASA.real.time.Satliate.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaRealTimeSatliateDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaRealTimeSatliateDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Satliate/NASA/real/time/Satliate/Data/NasaRealTimeSatliateDataApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Satliate.NASA.real.time.Satliate.Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NasaRealTimeSatliateDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
